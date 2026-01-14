d3877ff15a9b2ad02ec7d03d4505bfbcc77af020
abjshawty <kouadiobhegnino@gmail.com>
2026-01-14 18:25:55 +0000

    fix

--- lua/.wezterm.lua
+++ lua/.wezterm.lua
@@ -3,9 +3,9 @@ local config = wezterm.config_builder()
 
 config.font_size = 10
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE" -- "TITLE | RESIZE"
 config.color_scheme = 'Tokyo Night'
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.5
 config.default_prog = { 'powershell.exe', '-NoLogo' }
 config.front_end = 'WebGpu'
 config.keys = {
@@ -28,6 +28,11 @@ config.keys = {
 		key = 'w',
 		mods = 'CTRL',
 		action = wezterm.action.CloseCurrentPane { confirm = true },
+	},
+	{
+		key = 'Tab',
+		mods = 'CTRL',
+		action = wezterm.action.PaneSelect,
 	}
 }
 
