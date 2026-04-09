316354d3f46a446a4fa60cf82e3a0eee30d7d6c0
Timmy <kouadiobhegnino@gmail.com>
2026-04-09 00:25:19 +0000

    Added colorscheme for neovim

---
+++ lua/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+    { "ellisonleao/gruvbox.nvim", lazy = true },
+    { "folke/tokyonight.nvim",    lazy = true },
+    { "catppuccin/nvim",          lazy = true },
+    { "tahayvr/matteblack.nvim",  lazy = true },
+    { "shaunsingh/nord.nvim",     lazy = true },
+    { "sainnhe/everforest",       lazy = true },
+    { "bjarneo/aether.nvim",      lazy = true },
+    { "bjarneo/ethereal.nvim",    lazy = true },
+    { "bjarneo/hackerman.nvim",   lazy = true },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "matteblack",
+        },
+    },
+}
